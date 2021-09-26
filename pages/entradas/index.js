0f3516d5c0d76043c4344f3c9bec8c924bc5ce2b
elias <bianchi.elias@gmail.com>
2021-09-26 20:57:36 -0300

    entradas

--- pages/entradas/index.js
+++ pages/entradas/index.js
@@ -1,7 +1,26 @@
-const Entradas = () => {
+import Styles from '../../styles/Proyectos.module.css'
+import Link from 'next/link'
+
+export const getStaticProps = async () =>{
+    const res = await fetch('http://localhost/wordpress/wp-json/wp/v2/posts/');
+    const data = await res.json();
+
+    return {
+        props : { entradas: data }
+    }
+}
+
+const Entradas = ( { entradas } ) => {
     return ( 
         <div>
             <h2>Entradas</h2>
+            {entradas.map(entrada => (
+                <Link href={'/entradas/' + entrada.id} key={entrada.id}>
+                    <a className={Styles.single}>
+                        <h3> { entrada.date } - { entrada.slug } </h3>
+                    </a>
+                </Link>
+            ))}
         </div>
     );
 }
