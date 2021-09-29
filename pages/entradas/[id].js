9cbfb6e6538fd71ab99d7d2cfd64f639763c1ae3
elias <bianchi.elias@gmail.com>
2021-09-28 23:05:47 -0300

    finalizado

--- pages/entradas/[id].js
+++ pages/entradas/[id].js
@@ -1,15 +1,11 @@
-import Image from 'next/image'
-
 export const getStaticPaths = async () =>{
     const res = await fetch('http://localhost/wordpress/wp-json/wp/v2/posts/');
     const data = await res.json();
 
     const paths = data.map(entrada =>{
-        return {
-            params : { id: entrada.id.toString() }
-        }
-        
+        return {params : { id: entrada.id.toString() }}
     })
+
     return {
         paths,
         fallback: false
@@ -17,37 +13,58 @@ export const getStaticPaths = async () =>{
 }
 
 export const getStaticProps = async (context) => {
-    const id = context.params.id;
+    const id = context.params.id;  //POST ID
     
     const resPost = await fetch('http://localhost/wordpress/wp-json/wp/v2/posts/' + id + "?_embed=1");
     const dataPost = await(resPost.json());
-    
+
     const resMedia = await fetch('http://localhost/wordpress/wp-json/wp/v2/media/' + dataPost.featured_media);
     const dataMedia = await(resMedia.json());
+
+    const resCategories = await fetch('http://localhost/wordpress/wp-json/wp/v2/categories?post=' + id);
+    const dataCategories = await(resCategories.json());
+
+    const resTags = await fetch('http://localhost/wordpress/wp-json/wp/v2/tags?post=' + id);
+    const dataTags = await(resTags.json());
+
+    const resAuthor = await fetch('http://localhost/wordpress/wp-json/wp/v2/users/' + dataPost.author);
+    const dataAuthor = await(resAuthor.json());
     
     return {
-        props: {entrada: dataPost, entradaFoto: dataMedia}
+        props: {entrada: dataPost, entradaFoto: dataMedia, entradaCategories: dataCategories, entradaTags: dataTags, entradaAuthor: dataAuthor}
     }
 }
 
-const Details = ( { entrada, entradaFoto } ) => {
+const Details = ( { entrada, entradaFoto, entradaCategories, entradaTags, entradaAuthor } ) => {
     return ( 
         <div>
             <h1>{entrada.title.rendered}</h1>
+            {/* FECHA */}
             <p><strong>Fecha: </strong>{entrada.date}</p>
+            {/* TITULO */}
             <p><strong>Titulo: </strong>{entrada.title.rendered}</p>
-
+            {/* TEXTO */}
             <p><strong>Texto: </strong></p>
-            
             <div dangerouslySetInnerHTML={{__html: entrada.content.rendered}}></div>
-            
-            <p><strong>Categoria: </strong>{entrada.categories}</p>
-            <p><strong>Tags: </strong>{entrada.tags}</p>
-
+            {/* CATEGORIAS */}
+            <p><strong>Categorias: </strong></p>
+            <ul>
+                {entradaCategories.map(categoria =>(
+                    <li>{categoria.name}</li>
+                ))}
+            </ul>
+            {/* TAGS */}
+            <p><strong>Tags: </strong></p>
+            <ul>
+                {entradaTags.map(tag => (
+                    <li>{tag.name}</li>
+                ))}
+            </ul>
+            {/* FOTO */}
             <p><strong>Foto: </strong></p>
             <img src={entradaFoto.source_url} width={100} />
-
-            <p><strong>Autor: </strong>{entrada.author}</p>
+            {/* AUTOR */}
+            <p><strong>Autor: </strong>{entradaAuthor.name}</p>
         </div>
     );
 }
